da158a6e88dcaca0c7ddb72ccc0ae6c0fabea112
joshnaganja <119840321+joshnaganja@users.noreply.github.com>
2024-01-08 09:30:15 +0530

    import java.util.Scanner; public class MyClass {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int t;         t=a;         a=b;         b=t;         System.out.println(a);         System.out.println(b);     } }

---
+++ Swap_Two_Numbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class MyClass
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int t;
+        t=a;
+        a=b;
+        b=t;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
