c5ad03eed583ba4bf0a8856ee2d5ff4d702bf953
joshnaganja <119840321+joshnaganja@users.noreply.github.com>
2024-02-05 10:35:08 +0530

    import java.util.Scanner; public class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int m=sc.nextInt();         int z=(n*5)+(m*7);         System.out.println(z);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int z=(n*5)+(m*7);
+        System.out.println(z);
+    }
+}
